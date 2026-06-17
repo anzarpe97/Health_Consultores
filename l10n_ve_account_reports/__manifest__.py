545c934b9779a3b09a4dc6226b2a9a91f9970aa4
Dany-creator <98239032+Dany-creator@users.noreply.github.com>
2026-06-17 15:25:12 -0400

    modificaciones

--- l10n_ve_account_reports/__manifest__.py
+++ l10n_ve_account_reports/__manifest__.py
@@ -11,9 +11,11 @@
     'category': 'Accounting/Localizations',
     'version': '17.0.1.0',
     'depends': ['account_reports'],
-    'data': [
-        'views/account_reports_pdf_inherit.xml',
-    ],
+    'assets': {
+        'web.report_assets_pdf': [
+            'l10n_ve_account_reports/static/src/scss/account_report_print.scss',
+        ],
+    },
     'installable': True,
     'auto_install': False,
 }
