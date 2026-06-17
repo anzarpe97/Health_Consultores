8a7b9d6d88a9fec474f1e41db96735fb067b5ce5
Anderson Salazar <anzarpe10@gmail.com>
2026-06-17 09:41:46 -0400

    Implementacion modulo

---
+++ custom_report_layout/manifest.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+{
+    'name': 'Personalización de Encabezado Estándar',
+    'summary': 'Modifica el encabezado global de los informes de Odoo',
+    'version': '1.0',
+    'category': 'Technical',
+    'author': 'Tu Nombre',
+    'depends': ['web'],
+    'data': [
+        'views/report_templates.xml',
+    ],
+    'installable': True,
+    'application': False,
+}
