d6a6de05c84e40ed54e3a63da336227d64f84c45
Anderson Salazar <anzarpe10@gmail.com>
2026-06-17 10:09:40 -0400

    cambios

--- custom_report_layout/__manifest__.py
+++ custom_report_layout/__manifest__.py
@@ -2,14 +2,14 @@
 {
     'name': 'Custom External Layout (Odoo 17)',
     'summary': 'Modificaciones globales para el encabezado estándar de los reportes',
-    'version': '17.0.1.0.0',  # Formato oficial de Odoo 17
+    'version': '17.0.1.0.0',
     'category': 'Technical',
     'author': 'Anderson Salazar',
-    'depends': ['web'],       # Dependencia obligatoria de Odoo Web
+    'depends': ['web'],
     'data': [
         'views/report_templates.xml',
     ],
     'installable': True,
     'application': False,
-    'license': 'LGPL-3',      # Recomendado en Odoo 17
+    'license': 'LGPL-3',
 }
