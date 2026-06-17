e73c5d0d863d09b429b26ae843bb0ad6fca1ca75
Dany-creator <98239032+Dany-creator@users.noreply.github.com>
2026-06-17 15:44:20 -0400

    now

--- l10n_ve_account_reports/__init__.py
+++ l10n_ve_account_reports/__init__.py
@@ -1,7 +1,1 @@
-from odoo import api
-from odoo.exceptions import UserError
-
-def post_init_hook(env):
-    views = env['ir.model.data'].search([('module', '=', 'account_reports'), ('model', '=', 'ir.ui.view')])
-    names = views.mapped('name')
-    raise UserError(f"PLANTILLAS DISPONIBLES: {', '.join(names)}")
+from . import models
