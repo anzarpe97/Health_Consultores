c703013c33c25521a91db2e0933f92acc59393bc
Daniel Rodriguez <98239032+Dany-creator@users.noreply.github.com>
2026-06-17 21:14:12 -0400

    Update main.py

--- l10n_ve_account_reports/controllers/main.py
+++ l10n_ve_account_reports/controllers/main.py
@@ -3,21 +3,13 @@ from odoo.http import request
 import json
 
 class DebugController(http.Controller):
-    @http.route('/debug/account_reports_info', type='http', auth='public')
-    def get_info(self, **kwargs):
-        # Buscar todas las vistas/plantillas del módulo account_reports
-        views = request.env['ir.ui.view'].sudo().search_read(
-            [('key', '=like', 'account_reports.%')],
-            ['key', 'name', 'type', 'arch']
-        )
+    @http.route('/debug/account_reports_info_combined', type='http', auth='public')
+    def get_info_combined(self, **kwargs):
+        # Obtener la vista combinada (con todas las herencias aplicadas)
+        view = request.env.ref('account_reports.pdf_export_main')
+        arch = view.sudo()._get_combined_arch()
         
-        # Filtrar solo aquellas que parezcan estar relacionadas con PDFs, layouts o headers
-        relevant_views = []
-        for v in views:
-            if 'pdf' in v['key'] or 'print' in v['key'] or 'layout' in v['key'] or 'header' in v['key'] or 'template' in v['key']:
-                relevant_views.append(v)
-                
         return request.make_response(
-            json.dumps(relevant_views, indent=4),
-            headers=[('Content-Type', 'application/json')]
+            arch,
+            headers=[('Content-Type', 'text/xml')]
         )
