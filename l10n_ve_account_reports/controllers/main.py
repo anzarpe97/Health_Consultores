f12d2de5d0d85cc824062276a1060f2f9bc1f512
Daniel Rodriguez <98239032+Dany-creator@users.noreply.github.com>
2026-06-17 21:17:31 -0400

    Update main.py

--- l10n_ve_account_reports/controllers/main.py
+++ l10n_ve_account_reports/controllers/main.py
@@ -5,11 +5,13 @@ import json
 class DebugController(http.Controller):
     @http.route('/debug/account_reports_info_combined', type='http', auth='public')
     def get_info_combined(self, **kwargs):
+        from lxml import etree
         # Obtener la vista combinada (con todas las herencias aplicadas)
         view = request.env.ref('account_reports.pdf_export_main')
-        arch = view.sudo()._get_combined_arch()
+        arch_etree = view.sudo()._get_combined_arch()
+        arch_str = etree.tostring(arch_etree, encoding='unicode')
         
         return request.make_response(
-            arch,
+            arch_str,
             headers=[('Content-Type', 'text/xml')]
         )
