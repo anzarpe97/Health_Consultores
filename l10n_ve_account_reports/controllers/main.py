1d66ba8280a54abc85af392429c608ac5bdc2dd2
Daniel Rodriguez <98239032+Dany-creator@users.noreply.github.com>
2026-06-17 21:43:10 -0400

    more

--- l10n_ve_account_reports/controllers/main.py
+++ l10n_ve_account_reports/controllers/main.py
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*-
 from odoo import http
 from odoo.http import request
 import json
@@ -5,17 +6,10 @@ import json
 class DebugController(http.Controller):
     @http.route('/debug/account_reports_info_combined', type='http', auth='public')
     def get_info_combined(self, **kwargs):
-        import odoo
-        import os
-        module_path = odoo.modules.get_module_path('account_reports')
-        target_file = os.path.join(module_path, 'models', 'account_report.py')
-        try:
-            with open(target_file, 'r', encoding='utf-8') as f:
-                content = f.read()
-        except Exception as e:
-            content = str(e)
-            
-        return request.make_response(
-            content,
-            headers=[('Content-Type', 'text/plain')]
-        )
+        # Render the template to string
+        html = request.env['ir.qweb']._render('account_reports.company_information', {
+            'options': {'companies': [{'name': 'TEST COMPANY'}]},
+            'env': request.env,
+            'company': request.env.company,
+        })
+        return request.make_response(html, headers=[('Content-Type', 'text/html')])
