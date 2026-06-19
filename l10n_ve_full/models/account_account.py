89a84c0a1aa6b7b72b8e5857ce2bd7eed7728d0a
Dany-creator <98239032+Dany-creator@users.noreply.github.com>
2026-06-19 08:07:57 -0400

    eliminar

--- l10n_ve_full/models/account_account.py
+++ l10n_ve_full/models/account_account.py
@@ -2,36 +2,3 @@ from odoo import fields, models
 
 class AccountAccount(models.Model):
     _inherit = 'account.account'
-
-    def _auto_init(self):
-        # Fix for UndefinedColumn during server restart before module upgrade
-        self.env.cr.execute("""
-            ALTER TABLE account_account 
-            ADD COLUMN IF NOT EXISTS custom_inventory_type VARCHAR;
-        """)
-        return super(AccountAccount, self)._auto_init()
-
-    custom_inventory_type = fields.Selection(
-        selection=[
-            ('asset_receivable', 'Receivable'),
-            ('asset_cash', 'Bank and Cash'),
-            ('asset_current', 'Inventarios'),
-            ('asset_non_current', 'Non-current Assets'),
-            ('asset_prepayments', 'Prepayments'),
-            ('asset_fixed', 'Fixed Assets'),
-            ('liability_payable', 'Payable'),
-            ('liability_credit_card', 'Credit Card'),
-            ('liability_current', 'Current Liabilities'),
-            ('liability_non_current', 'Non-current Liabilities'),
-            ('equity', 'Equity'),
-            ('equity_unaffected', 'Current Year Earnings'),
-            ('income', 'Income'),
-            ('income_other', 'Other Income'),
-            ('expense', 'Expenses'),
-            ('expense_depreciation', 'Depreciation'),
-            ('expense_direct_cost', 'Cost of Revenue'),
-            ('off_balance', 'Off-Balance Sheet'),
-        ],
-        string="Inventarios",
-        help="Tipo de cuenta personalizado para Inventarios"
-    )
